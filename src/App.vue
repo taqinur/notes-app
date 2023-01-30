acf406fb0cf1bbc45625b717ba173efb161ce048
Taqinur <taqi1662@gmail.com>
2023-01-30 12:21:36 +0600

    header added

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-
 </script>
 
 <template>
-
+  <main>
+    <div class="container">
+      <header>
+        <h1>Notes App</h1>
+      </header>
+    </div>
+  </main>
 </template>
 
 <style scoped>
-
 </style>
